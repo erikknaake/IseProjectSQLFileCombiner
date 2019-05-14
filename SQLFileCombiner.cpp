10aa9fc8a28a75b0124bbbcdb00677e787f96699
Erik Knaake <e.knaake@student.han.nl>
2019-05-14 10:31:40 +0200

    Filter out USE statements after the first one

--- SQLFileCombiner.cpp
+++ SQLFileCombiner.cpp
@@ -2,40 +2,61 @@
 #include <experimental/filesystem>
 #include <fstream>
 #include <string>
+#include <map>
 
 namespace fs = std::experimental::filesystem;
 
 int main()
 {
 	// Output file, clear before use
-	std::ofstream testOutput("Root/testFile.sql", std::fstream::trunc);
-	std::ofstream sqlOutput("Root/sqlFile.sql", std::fstream::trunc);
+	const char* sqlPath = "sqlFile.sql";
+	const char* createScript = "create.sql";
+	const char* testFile = "testFile.sql";
+	const char* use = "USE IseProject";
 
-	std::fstream createFile("Root/create.sql");
+	std::ofstream testOutput(testFile, std::fstream::trunc);
+	std::ofstream sqlOutput(sqlPath, std::fstream::trunc);
+	std::fstream createFile(createScript);
+
 	sqlOutput << createFile.rdbuf();
+	testOutput << "USE IseProject\n";
 
-	for (auto& file : fs::recursive_directory_iterator("Root"))
+	for (auto& file : fs::recursive_directory_iterator("./"))
 	{
+		if (file.path().u8string().find(".sql") == std::string::npos)
+			continue;
 		std::cout << file.path().u8string() << std::endl;
+
+		std::string line;
+		std::fstream currentFile(file);
+
+		if (!currentFile.is_open())
+		{
+			std::cout << file.path() << " could not be opened" << std::endl;
+			break;
+		}
+
 		if (file.path().u8string().find(".spec") != std::string::npos)
 		{
-			std::string line;
-			std::fstream currentFile(file);
-
-			if (!currentFile.is_open())
-			{
-				std::cout << file.path() << " could not be opened" << std::endl;
-				break;
-			}
 			while (std::getline(currentFile, line))
 			{
-				if (line.find("EXEC tSQLt.Run") == std::string::npos)
+				if (line.find("EXEC tSQLt.Run") == std::string::npos && line.find(use) == std::string::npos)
 					testOutput << line << std::endl;
 			}
 		}
-		else if (file.path().u8string().find("sqlFile.sql") == std::string::npos && file.path().u8string().find("create.sql") == std::string::npos)
-			sqlOutput << std::fstream(file).rdbuf();
+		else if (file.path().u8string().find(sqlPath) == std::string::npos 
+			&& file.path().u8string().find(createScript) == std::string::npos
+			&& file.path().u8string().find(testFile) == std::string::npos)
+		{
+			while (std::getline(currentFile, line))
+			{
+				if (line.find(use) == std::string::npos)
+					sqlOutput << line << std::endl;
+			}
+		}	
 	}
 
 	testOutput << "\nEXEC tSQLt.RunAll\n";
+	sqlOutput.close();
+	testOutput.close();
 }
